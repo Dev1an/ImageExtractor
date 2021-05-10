f640f6a90c1efeac0e5c18ef1ca3edfa2ad8e68d
Damiaan Dufaux <damiaan@dpro.be>
2021-05-10 17:56:42 +0200

    Add tool name

--- Sources/CommandLineTools/MainTool.swift
+++ Sources/CommandLineTools/MainTool.swift
@@ -11,6 +11,7 @@ public struct MainTool: ParsableCommand {
 	public init() {}
 
 	public static let configuration = CommandConfiguration(
+		commandName: "ImageExtractor",
 		abstract: "A utility to extract original images from PDFs",
 		discussion: "ImageExtracor gives you acces to the original image representations of bitmap assets in PDFs. It does not rerender, resample, or scale the images, but provides you the actual representations of the images, just as they are encoded in the PDF",
 		subcommands: [List.self, Save.self],
